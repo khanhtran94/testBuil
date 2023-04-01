eb92e81cd6ba5c559941dcc6b283d860229ed919
khanhTran <khanhtrantung190894@gmail.com>
2023-04-01 22:25:08 +0700

    init

---
+++ src/main/java/com/example/testbuid/TestBuidApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbuid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBuidApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/testbuid/controller/TestController.java
@@ -0,0 +1,2 @@
+package com.example.testbuid.controller;public class TestController {
+}
